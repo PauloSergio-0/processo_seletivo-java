e823e879ebbe3f0e8f8ee39f9eee1c7d3e7703c9
Paulo Sérgio <paulosergiosrs08@gmail.com>
2025-03-14 12:31:47 -0300

    featadded array candidatos

--- src/edu/paulo/challengeprocessoseletivo/ProcessoSeletivo.java
+++ src/edu/paulo/challengeprocessoseletivo/ProcessoSeletivo.java
@@ -3,10 +3,13 @@ package edu.paulo.challengeprocessoseletivo;
 public class ProcessoSeletivo {
 
     public static void main(String[] args) {
-        double salarioCandidato = 2000;
+        double[] salarioCandidato = {2000,500.98,400.21,1236.90, 6000.21,900.90,299};
+        String[] nomesCandidatos = {"Allan", "Will", "Joeo", "Rilson","Mickey", "Boston", "willys"};
 
         SalarioValidator validadorCandidato = new SalarioValidator();
 
-        validadorCandidato.ValidarSalario(salarioCandidato);
+        validadorCandidato.ValidarSalario(salarioCandidato,nomesCandidatos);
+
+        validadorCandidato.candidatosAprovados();
     }
 }
--- src/edu/paulo/challengeprocessoseletivo/SalarioValidator.java
+++ src/edu/paulo/challengeprocessoseletivo/SalarioValidator.java
@@ -1,17 +1,44 @@
 package edu.paulo.challengeprocessoseletivo;
-
+import java.util.ArrayList;
 
 class SalarioValidator {
     double salario = 2000.0;
+    ArrayList<String> nomes= new ArrayList<>(); 
 
-    public void ValidarSalario(double  salarioPretendido){
+    public void ValidarSalario(double[] salarioPretendido, String[] listaCandidato){
 
-        if(salarioPretendido < this.salario){
-            System.out.println("ligar para candidato");
-        } else if (salarioPretendido == this.salario){
-            System.out.println("Ligar para os demais candidatos");
-        } else if (salarioPretendido > this.salario){
-            System.out.println("Aguardar o resultado dos demais candidatos");
+        if(salarioPretendido.length != listaCandidato.length){
+            System.out.println("Listas com tamanhos diferentes");
+            return;
+        }
+
+        for(int i=0; i < salarioPretendido.length; i++){
+            if (this.nomes.size() ==5) {
+                System.out.println("Máximo de candidatos aceitos fim da seleção");
+                return;
+            }
+
+            if(salarioPretendido[i] < this.salario){
+                System.out.println("ligar para candidato: "+ listaCandidato[i]);
+                nomes.add(listaCandidato[i]);
+
+            } else if (salarioPretendido[i] == this.salario && listaCandidato.length != 5){
+                System.out.println("Ligar para os demais, candidato: "+listaCandidato[i]);
+                nomes.add(listaCandidato[i]);
+            
+            } else if (salarioPretendido[i] > this.salario){
+                System.out.println("Aguardar o resultado dos demais, candidato: "+ listaCandidato[i]);
+            }}
+    }
+
+    public void candidatosAprovados(){
+        if(this.nomes.size() >0){
+            System.out.println("\nCanditos aprovados:");
+
+            for(String candidato: this.nomes){
+                System.out.println(candidato);
+            }
         }
     }
+
 }
